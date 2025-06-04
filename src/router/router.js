382791f0d6341d7aa7a1a23bfea7669dd8ad14e8
Thomas Mach <m4chtomasz@gmail.com>
2025-06-04 11:42:07 +0200

    fix router

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 // import { useAuthStore } from "../store/storeAuth.js"; // Store di autenticazione
 import Signup from "../views/Singup.vue";
 import Signin from "../views/Signin.vue";
@@ -120,7 +120,7 @@ const routes = [
 // ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
